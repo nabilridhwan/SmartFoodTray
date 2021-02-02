326c9c55d47efaef205f8e4edf786c60b639af7c
Nabil Ridhwan <nabridhwan@gmail.com>
2021-02-02 20:26:25 +0800

    Fixed bug where Done will loop unless there is a send collected message

--- tray.py
+++ tray.py
@@ -10,22 +10,36 @@ alert_tune = ["F#6"]
 tiltThreshold = 300
 
 (receiveDone, receiveCollected) = ("Done", "Collected")
-isDone = False
+
+lastReceived = ""
 
 while True:
     (readingX, readingY) = (accelerometer.get_x(), accelerometer.get_y())
     incoming = radio.receive()
     
     if incoming == receiveDone:
-        isDone = True
-    elif incoming == receiveCollected:
-        isDone = False
-        
-    while isDone:
         display.clear()
-        music.play(alert_tune)
-        display.scroll("Done", 75)
+        lastReceived = receiveDone
+    #     music.play(alert_tune)
+    #     display.scroll("Done", delay=75)
+    #     sleep(100)
+    elif incoming == receiveCollected:
+        lastReceived = receiveCollected
+    #     display.clear()
+    #     display.scroll("Collected",delay=75)
 
+    # itemIsDone = incoming == receiveDone
+        
+    # while itemIsDone:
+        # music.play(alert_tune)
+        # display.scroll("Done", delay=75)
+        # sleep(100)
+    
+    if lastReceived == receiveDone:
+        music.play(alert_tune)
+        display.scroll("Done", delay=75)
+        sleep(100)
+    
     if readingX > tiltThreshold:
         display.show(Image.ARROW_W)
         music.play(alert_tune)
