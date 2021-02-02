e07ecf4596058a6af09de298c8ef76bfc21e1f15
Nabil Ridhwan <nabridhwan@gmail.com>
2021-02-02 19:10:36 +0800

    Renamed files and fixed few bugs

---
+++ stall.py
@@ -0,0 +1,40 @@
+from microbit import *
+import radio, music
+
+radio.on()
+channel = 1
+radio.config(channel=channel)
+tune = ["Eb6"]
+buttonBCount = 0
+# tune1 = ["A5:1"]
+# tune2 = ["Gb5:1"]
+
+(sendDone, sendCollected) = ("Done", "Collected")
+
+def sendDoneSignal():
+    music.play(tune1)
+    radio.send(sendCollected)
+    display.scroll("Done",50)
+
+def sendCollectedSignal():
+    music.play(tune2)
+    radio.send(sendDone)
+    display.scroll("Collected",50)
+
+while True:
+    if button_a.is_pressed():
+        channel = channel + 1
+        display.show(channel)
+        # music.play(tune)
+
+        if channel >= 20:
+            channel = 1
+    # Button B is pressed, it will send a done signal
+    elif button_b.is_pressed():
+        buttonBCount = buttonBCount + 1
+    
+    if buttonBCount % 2 != 0:
+        sendDoneSignal()
+    else:
+        sendCollectedSignal()
+        
--- tray.py
+++ tray.py
@@ -7,33 +7,35 @@ radio.on()
 radio.config(channel=tray_id)
 alert_tune = ["F#6"]
 
-threshold = 300
+tiltThreshold = 300
 
-doneRadioReceive = 'd'
-collectedRadioReceive = 'c'
+(receiveDone, receiveCollected) = ("Done", "Collected")
+isDone = False
 
-while True
-check = radio.receive()
-
-if check is doneRadioReceive:
-    display.clear()
-    for i in range(0, 10):
-        music.play(alert_tune)
-        display.scroll("Done",75)
-#if check is collectedRadioReceive:
-else:
+while True:
     (readingX, readingY) = (accelerometer.get_x(), accelerometer.get_y())
+    incoming = radio.receive()
+    
+    if incoming == receiveDone:
+        isDone = True
+    elif incoming == receiveCollected:
+        isDone = False
+        
+    while isDone:
+        display.clear()
+        music.play(alert_tune)
+        display.scroll("Done", 75)
 
-    if readingX > threshold:
+    if readingX > tiltThreshold:
         display.show(Image.ARROW_W)
         music.play(alert_tune)
-    elif readingX < -threshold:
+    elif readingX < -tiltThreshold:
         display.show(Image.ARROW_E)
         music.play(alert_tune)
-    elif readingY > threshold:
+    elif readingY > tiltThreshold:
         display.show(Image.ARROW_N)
         music.play(alert_tune)
-    elif readingY < -threshold:
+    elif readingY < -tiltThreshold:
         display.show(Image.ARROW_S)
         music.play(alert_tune)
     else:
